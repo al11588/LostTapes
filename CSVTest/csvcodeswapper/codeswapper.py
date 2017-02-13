4a693405987c57440cd115ceaf325d95b812ba5a
Alvin Lawson <al11588@gmail.com>
2017-02-13 10:19:41 -0500

    cleaning some code

--- CSVTest/csvcodeswapper/codeswapper.py
+++ CSVTest/csvcodeswapper/codeswapper.py
@@ -1,10 +1,12 @@
 import csv 
-
-with open('original.csv') as originalcsv: #reads the original CSV
+#reads the original CSV
+with open('original.csv') as originalcsv: 
+	#adds the delimeter to the file
 	reader = csv.reader(originalcsv, delimiter=',')
-	with open('final.csv', 'wb') as finalcsv: #opens the final CSV
+	#opens the final CSV
+	with open('final.csv', 'wb') as finalcsv: 
 		writer = csv.writer(finalcsv)
-		counter = 0
+		
 		for row in reader:
 			row[0] = row[1] #splits the rows and are organized
 			writer.writerow(row)
