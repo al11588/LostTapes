c6d2fb0c609fd61cf5df5df8368f156f23686d15
Alvin Lawson <al11588@gmail.com>
2017-02-25 12:03:06 -0500

    adding mean median and mode

---
+++ MathTest/meanmedianmode/mean.py
@@ -0,0 +1,13 @@
+import statistics
+
+
+numbers = [3,2,88,77, 77]
+
+mean = statistics.mean(numbers)
+
+median = statistics.median(numbers)
+
+mode = statistics.mode(numbers)
+
+print ("The mean is :", mean, "The median is :", median,"The mode is:", mode)
+
