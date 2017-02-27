a323c6dab54a977708041c3714e95890d680f2b5
Alvin Lawson <al11588@gmail.com>
2017-02-27 13:42:57 -0500

    adding a new math

---
+++ MathTest/projecteuler/problem1.py
@@ -0,0 +1,10 @@
+multipleofthree = sum(range(0, 1000, 3))
+
+multipleoffive = sum(range(0, 1000, 5))
+
+multipleoffifteen = sum(range(0, 1000, 15))
+
+final = multipleofthree + multipleoffive - multipleoffifteen
+
+print final 
+
